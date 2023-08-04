bfbe014ff56161b738ae069d8cc5022669c8067d
igrok1803444 <v618591@gmail.com>
2023-08-04 10:55:17 +0300

    finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let markup =  [];
+ingredients.forEach((ingredient) => {
+  const li = document.createElement('li')
+li.className = 'item';
+li.textContent = ingredient;
+  markup.push(li);
+})
+console.log(markup);
+const list = document.querySelector('#ingredients');
+list.append(...markup)
