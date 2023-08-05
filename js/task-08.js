25b7f710a89b1996e7b3ae602711fb09fbe57f8f
igrok1803444 <v618591@gmail.com>
2023-08-05 14:12:34 +0300

    finish task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+const submitButton = document.querySelector('button[type="submit"]');
 
+const formValues = {};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { password, email } } = event.currentTarget;
+    if (password.value === "" || email.value === "") {
+        alert("Please fill all fields")
+    } else {
+        formValues.email = email.value;
+        formValues.password = password.value;
+
+        console.log(formValues);
+        form.reset();
+    }
+})
